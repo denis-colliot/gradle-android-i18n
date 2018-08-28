b1bd3e92f5627a773ceb654d7fc458d5f44a00c2
Denis Colliot <denis.colliot@ratp.fr>
2018-08-28 17:38:20 +0200

    Minor refactoring

--- src/main/kotlin/com/github/gradle/android/i18n/AndroidI18nPlugin.kt
+++ src/main/kotlin/com/github/gradle/android/i18n/AndroidI18nPlugin.kt
@@ -9,28 +9,26 @@ import org.gradle.api.Project
  */
 class AndroidI18nPlugin : Plugin<Project> {
 
-    override fun apply(project: Project?) {
-        project?.let {
+    override fun apply(project: Project) {
 
-            val extension = project.extensions.create("androidI18n",
-                    AndroidI18nPluginExtension::class.java,
-                    XlsImporter(project))
+        val extension = project.extensions.create("androidI18n",
+                AndroidI18nPluginExtension::class.java,
+                XlsImporter(project))
 
-            project.tasks.let {
-                it.create("androidI18nImport").apply {
-                    doLast {
-                        project.logger.info("Importing android i18n resources")
-                        extension.importI18nResources()
-                    }
+        project.tasks.let { task ->
+            task.create("androidI18nImport").apply {
+                doLast {
+                    project.logger.info("Importing android i18n resources")
+                    extension.importI18nResources()
                 }
             }
+        }
 
-            project.tasks.let {
-                it.create("androidI18nExport").apply {
-                    doLast {
-                        project.logger.info("Exporting android i18n resources")
-                        extension.exportI18nResources()
-                    }
+        project.tasks.let { task ->
+            task.create("androidI18nExport").apply {
+                doLast {
+                    project.logger.info("Exporting android i18n resources")
+                    extension.exportI18nResources()
                 }
             }
         }
