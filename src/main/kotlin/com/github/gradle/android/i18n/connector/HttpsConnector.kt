416d438ddaefac2562193f8567087626727310e3
geekarist <geekarist@users.noreply.github.com>
2020-03-31 09:44:36 +0200

    Refactor HttpsConnector
    Extract functions, write documentation

--- src/main/kotlin/com/github/gradle/android/i18n/connector/HttpsConnector.kt
+++ src/main/kotlin/com/github/gradle/android/i18n/connector/HttpsConnector.kt
@@ -7,23 +7,38 @@ import java.nio.charset.Charset
 import java.util.*
 import javax.net.ssl.HttpsURLConnection
 
+/**
+ * This connector allows to read a file via HTTPS with basic auth.
+ *
+ * Here is how to use it:
+ *
+ * ```
+ * val connector = HttpsConnector("https://foo:bar@example.com/file.dat")
+ * val bytes = connector.intputStream.readBytes()
+ * ```
+ *
+ * @param url The URL of the file to read. It has to respect this pattern:
+ * `https://user:password@path`.
+ */
 class HttpsConnector(url: String) {
 
-    private val connection: HttpsURLConnection = run {
-        val urlObj = URL(url)
-        val basicAuth = extractBasicAuth(urlObj)
-        val connection = urlObj.openConnection() as HttpsURLConnection
-        connection.addRequestProperty("Authorization", "Basic $basicAuth")
-        connection
-    }
-
-    private fun extractBasicAuth(urlObj: URL): String {
-        val urlEncodedCredentials = urlObj.userInfo
-        val credentials = URLDecoder.decode(urlEncodedCredentials, Charset.defaultCharset().name())
-        val credentialsBytes = credentials.toByteArray()
-        val encodedCredentials = Base64.getEncoder().encode(credentialsBytes)
-        return String(encodedCredentials)
-    }
+    private val connection: HttpsURLConnection = openConnection(url)
 
     val inputStream: InputStream = connection.inputStream
+}
+
+private fun openConnection(url: String): HttpsURLConnection {
+    val urlObj = URL(url)
+    val basicAuth = extractBasicAuth(urlObj)
+    val connection = urlObj.openConnection() as HttpsURLConnection
+    connection.addRequestProperty("Authorization", "Basic $basicAuth")
+    return connection
+}
+
+private fun extractBasicAuth(urlObj: URL): String {
+    val urlEncodedCredentials = urlObj.userInfo
+    val credentials = URLDecoder.decode(urlEncodedCredentials, Charset.defaultCharset().name())
+    val credentialsBytes = credentials.toByteArray()
+    val encodedCredentials = Base64.getEncoder().encode(credentialsBytes)
+    return String(encodedCredentials)
 }
