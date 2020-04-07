61e3ad2f2c4ac5bb74c7fce45d3039bc77417bf8
geekarist <geekarist@users.noreply.github.com>
2020-04-07 11:56:25 +0200

    Auto test export then import

--- src/test/kotlin/com/github/gradle/android/i18n/export/XlsExporterTest.kt
+++ src/test/kotlin/com/github/gradle/android/i18n/export/XlsExporterTest.kt
@@ -1,8 +1,11 @@
 package com.github.gradle.android.i18n.export
 
+import com.github.gradle.android.i18n.import.ImportConfig
+import com.github.gradle.android.i18n.import.XlsImporter
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Test
 import java.io.File
+import java.io.FileInputStream
 import java.io.FileOutputStream
 
 class XlsExporterTest {
@@ -21,5 +24,26 @@ class XlsExporterTest {
         outputStream.close()
         println("Exported file:\n${outputFile.path}")
         Runtime.getRuntime().exec("open ${outputFile.path}")
+    }
+
+    @Test
+    fun `should export then import`() {
+
+        val exportProjectDir = File("src/test/resources/export_then_import/export")
+        val exportProject = ProjectBuilder.builder().withProjectDir(exportProjectDir).build()
+        val exporter = XlsExporter(exportProject)
+        val exportOutputFile = File.createTempFile("i18n", ".xlsx")
+        val outputStream = FileOutputStream(exportOutputFile)
+        exporter.export(outputStream, "en")
+        outputStream.close()
+        println("Exported file:\n${exportOutputFile.path}")
+
+        val importProjectDir = File("build/output/import")
+        importProjectDir.mkdirs()
+        val importProject = ProjectBuilder.builder().withProjectDir(importProjectDir).build()
+        val importer = XlsImporter(importProject)
+        val inputStream = FileInputStream(exportOutputFile)
+        val config = ImportConfig("en", false, Regex(".*"))
+        importer.generate(inputStream, config)
     }
 }
