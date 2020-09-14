8e4f9d4db863ab7c01d8180237c12bd482fa3ccb
Christophe Pelé <geekarist@users.noreply.github.com>
2020-09-14 15:50:23 +0200

    Add comments to `model/*Data` classes

--- src/main/kotlin/com/github/gradle/android/i18n/model/TranslationData.kt
+++ src/main/kotlin/com/github/gradle/android/i18n/model/TranslationData.kt
@@ -1,3 +1,9 @@
 package com.github.gradle.android.i18n.model
 
+/**
+ * This class aggregates all the internationalized labels corresponding to a language.
+ * @param locale the name of the language eg `fr` or `en`
+ * @param stringDataList a list of translated labels. All of these are in the considered language:
+ * if [locale] is `fr`, all [StringData]s are in french.
+ */
 class TranslationData(val locale: String, val stringDataList: List<StringData>)
